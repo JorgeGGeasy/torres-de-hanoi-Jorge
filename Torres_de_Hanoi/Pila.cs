f1718734a9a348a8eada231644da721e48ca7c6f
Jorge <jgragia@epsg.upv.es>
2021-03-16 11:33:04 +0100

    Pila completado

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -8,35 +8,80 @@ namespace Torres_de_Hanoi
 {
     class Pila
     {
-        public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
-        public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
-        public List<Disco> Elementos { get; set; }
-        */
+        private int discos;
+        private int arriba;
+        private List<Disco> elementos;
 
-        /* TODO: Implementar métodos */
+
+
+        public int Size { 
+            get{  
+                return this.discos;
+            } 
+            
+            set{
+               this.discos = value;
+            } 
+        }
+
+        public int Top{
+            get{
+                return this.arriba;
+            } 
+            set{
+                this.arriba = value;
+            }
+        }
+
+        public List<Disco> Elementos {
+            get{
+                return this.elementos;
+            } 
+            set{
+                this.elementos = value;
+            }
+        }
+
+        public Pila(int discos, int arriba, List<Disco> elementos)
+        {
+            this.discos = discos;
+            this.arriba = arriba;
+            this.elementos = elementos;
+        }
+
         public Pila()
         {
-
+            this.discos = 0;
+            this.arriba = -1;
+            this.elementos = new List<Disco>();
         }
 
+        // Añadimos el disco a la pila
         public void push(Disco d)
         {
+                this.elementos.Add(d);
+                this.discos++;
+                this.arriba = d.Valor;
 
         }
-
+        
+        // Elimina el disco de la pila
         public Disco pop()
         {
-            return null;
+            Disco Dpop = this.elementos[this.elementos.Count-1]; 
+            this.elementos.RemoveAt(this.elementos.Count-1);
+            this.Size--;
+            this.Top = this.elementos[this.elementos.Count-1].Valor;
+            return Dpop;
         }                
-
+        
+        // Comprobamos si la lista esta vacia
         public bool isEmpty()
         {
-            return true;
+            if(elementos.Count == 0){
+                return true;
+            }
+            return false;
         }
 
     }
